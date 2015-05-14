2330d15578439f5ba99825b60c8a7b79fffde1b0
Andy <gxagxagxa@me.com>
2015-05-14 18:06:35 +0800

    implement _sqliter3d method  (red metadata needs to be done)

--- generaterawdb.py
+++ generaterawdb.py
@@ -39,6 +39,7 @@ class generaterawdb(object):
 
 
     def _sqliteari(self, arilist, dbpath):
+        self._symbolari(arilist)
         connect = sqlite3.connect(dbpath)
         try:
             cursor = connect.cursor()
@@ -48,8 +49,156 @@ class generaterawdb(object):
                     metadata = csv.reader(csvfile, delimiter=',')
                     for index, item in enumerate(metadata):
                         if index > 0:
-                            cursor.execute('INSERT INTO RAWMETADATA (FULLPATH, MASTER_TC, REEL) VALUES (?, ?, ?)',
-                                           (arilist[index - 1], item[1], item[2]))
+                            cursor.execute('INSERT INTO RAWMETADATA ('
+                                           'FULLPATH,'
+                                           'MASTER_TC,'
+                                           'REEL,'
+                                           'SCENE,'
+                                           'TAKE,'
+                                           'DIRECTOR,'
+                                           'CINEMATOGRAPHER,'
+                                           'PRODUCTION,'
+                                           'CIRCLE_TAKE,'
+                                           'PRODUCTION_COMPANY,'
+                                           'LOCATION,'
+                                           'OPERATOR,'
+                                           'User_Info_1,'
+                                           'User_Info_2,'
+                                           'Camera_Clip_Name,'
+                                           'Camera_Family,'
+                                           'Camera_Serial_Number,'
+                                           'Camera_ID,'
+                                           'Camera_Index,'
+                                           'Camera_SUP_Name,'
+                                           'Camera_Model,'
+                                           'Camera_Product,'
+                                           'Camera_SubProduct,'
+                                           'System_Image_Creation_Date,'
+                                           'Exposure_Time,'
+                                           'Shutter_Angle,'
+                                           'Mirror_Shutter_Running,'
+                                           'Sensor_FPS,'
+                                           'Project_FPS,'
+                                           'Master_TC_Time_Base,'
+                                           'Master_TC_Frame_Count,'
+                                           'Master_TC_User_Info,'
+                                           'Storage_Media_Serial_Number,'
+                                           'SMPTE_UMID,'
+                                           'Recorder_Type,'
+                                           'Vari,'
+                                           'UUID,'
+                                           'Image_Width,'
+                                           'Image_Height,'
+                                           'Active_Image_Width,'
+                                           'Active_Image_Height,'
+                                           'Active_Image_Top,'
+                                           'Active_Image_Left,'
+                                           'Full_Image_Width,'
+                                           'Full_Image_Height,'
+                                           'Color_Processing_Version,'
+                                           'White_Balance,'
+                                           'White_Balance_CC,'
+                                           'WB_Factor_R,'
+                                           'WB_Factor_G,'
+                                           'WB_Factor_B,'
+                                           'WB_Applied_In_Camera,'
+                                           'Exposure_Index_ASA,'
+                                           'Target_Color_Space,'
+                                           'Sharpness,'
+                                           'Lens_Squeeze,'
+                                           'Image_Orientation,'
+                                           'Look,'
+                                           'Look_Burned_In,'
+                                           'Look_LUT_Mode,'
+                                           'Look_LUT_Offset,'
+                                           'Look_LUT_Size,'
+                                           'Look_Saturation,'
+                                           'CDL_Slope_R,'
+                                           'CDL_Slope_G,'
+                                           'CDL_Slope_B,'
+                                           'CDL_Offset_R,'
+                                           'CDL_Offset_G,'
+                                           'CDL_Offset_B,'
+                                           'CDL_Power_R,'
+                                           'CDL_Power_G,'
+                                           'CDL_Power_B,'
+                                           'Printer_Lights_R,'
+                                           'Printer_Lights_G,'
+                                           'Printer_Lights_B,'
+                                           'CDL_Mode,'
+                                           'Lens_Model,'
+                                           'Lens_Serial_Number,'
+                                           'Lens_Distance_Unit,'
+                                           'Lens_Focus_Distance,'
+                                           'Lens_Focal_Length,'
+                                           'Lens_Iris,'
+                                           'Lens_Linear_Iris,'
+                                           'RawEncoderFocus_RawLds,'
+                                           'RawEncoderFocus_RawMotor,'
+                                           'RawEncoderFocal_RawLds,'
+                                           'RawEncoderFocal_RawMotor,'
+                                           'RawEncoderIris_RawLds,'
+                                           'RawEncoderIris_RawMotor,'
+                                           'EncoderLimFocusLdsMin,'
+                                           'EncoderLimFocusLdsMax,'
+                                           'EncoderLimFocusMotorMin,'
+                                           'EncoderLimFocusMotorMax,'
+                                           'EncoderLimFocalLdsMin,'
+                                           'EncoderLimFocalLdsMax,'
+                                           'EncoderLimFocalMotorMin,'
+                                           'EncoderLimFocalMotorMax,'
+                                           'EncoderLimIrisLdsMin,'
+                                           'EncoderLimIrisLdsMax,'
+                                           'EncoderLimIrisMotorMin,'
+                                           'EncoderLimIrisMotorMax,'
+                                           'Lds_Lag_Type,'
+                                           'Lds_Lag_Value,'
+                                           'ND_Filter_Type,'
+                                           'ND_Filter_Density,'
+                                           'Camera_Tilt,'
+                                           'Camera_Roll,'
+                                           'Master_Slave_Setup_Info,'
+                                           'S3D_Eye_Info,'
+                                           'Sound_Roll,'
+                                           'RAWTYPE'
+                                           ') VALUES ('
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                           (arilist[index - 1], item[1], item[2], item[3], item[4], item[5], item[6],
+                                            item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14],
+                                            item[15], item[16], item[17], item[18], item[19], item[20], item[21],
+                                            item[22],
+                                            item[23], item[24], item[25], item[26], item[27], item[28], item[29],
+                                            item[30],
+                                            item[31], item[32], item[33], item[34], item[35], item[36], item[37],
+                                            item[38],
+                                            item[39], item[40], item[41], item[42], item[43], item[44], item[45],
+                                            item[46],
+                                            item[47], item[48], item[49], item[50], item[51], item[52], item[53],
+                                            item[54],
+                                            item[55], item[56], item[57], item[58], item[59], item[60], item[60],
+                                            item[62],
+                                            item[63], item[64], item[65], item[66], item[67], item[68], item[69],
+                                            item[70],
+                                            item[71], item[72], item[73], item[74], item[75], item[76], item[77],
+                                            item[78],
+                                            item[79], item[80], item[81], item[82], item[83], item[84], item[85],
+                                            item[86],
+                                            item[87], item[88], item[89], item[90], item[91], item[92], item[93],
+                                            item[94],
+                                            item[95], item[96], item[97], item[98], item[99], item[100], item[101],
+                                            item[102],
+                                            item[103], item[104], item[105], item[106], item[107], item[108], item[109],
+                                            'ari'))
                     cursor.close()
                     connect.commit()
         except:
@@ -59,15 +208,106 @@ class generaterawdb(object):
             connect.close()
 
 
-    def _symbolr3d(self, r3dlist):
+    def _sqliter3d(self, r3dlist, dbpath):
+        r3dscv = os.path.join(self._temppath, os.path.basename(self._scanpath), 'R3D', 'R3D_metadata_output.csv')
+        print(r3dscv)
         for index, item in enumerate(r3dlist):
             try:
                 os.symlink(item,
-                           os.path.join(self._temppath, os.path.basename(self._scanpath), 'ari', '%08d.ari' % index))
+                           os.path.join(self._temppath, os.path.basename(self._scanpath), 'R3D', '%08d.R3D' % index))
+                cmd = 'REDline --i {0} --printMeta 2 >> {1}'.format(
+                    os.path.join(self._temppath, os.path.basename(self._scanpath), 'R3D', '%08d.R3D' % index), r3dscv)
+                subprocess.call(cmd, shell=True)
+
             finally:
-                print('something error in ari symbol link')
-        cmd = 'ARRIMetaExtract_CMD -i {0} -s \",\" -o {0}'.format(os.path.join(self._temppath, 'ari'))
-        subprocess.call(cmd, shell=True)
+                print('something error in R3D symbol link')
+
+        connect = sqlite3.connect(dbpath)
+        try:
+            # print(r3dlist)
+            cursor = connect.cursor()
+            if os.path.exists(r3dscv):
+                with open(r3dscv) as csvfile:
+                    metadata = csv.reader(csvfile, delimiter=',')
+                    for index, item in enumerate(metadata):
+                        if index %2 == 1:
+
+                            cursor.execute('INSERT INTO RAWMETADATA ('
+                                           'FULLPATH,'
+                                           'MASTER_TC,'
+                                           'REEL,'
+                                           'SCENE,'
+                                           'TAKE,'
+                                           'DIRECTOR,'
+                                           'CINEMATOGRAPHER,'
+                                           'PRODUCTION,'
+                                           'CIRCLE_TAKE,'
+                                           'PRODUCTION_COMPANY,'
+                                           'LOCATION,'
+                                           'OPERATOR,'
+                                           'Camera_Clip_Name,'
+                                           'Camera_Family,'
+                                           'Camera_Serial_Number,'
+                                           'Camera_ID,'
+                                           'Camera_Index,'
+                                           'Camera_SUP_Name,'
+                                           'Camera_Model,'
+                                           'Camera_Product,'
+                                           'Camera_SubProduct,'
+                                           'System_Image_Creation_Date,'
+                                           'Exposure_Time,'
+                                           'Shutter_Angle,'
+                                           'Mirror_Shutter_Running,'
+                                           'Sensor_FPS,'
+                                           'Project_FPS,'
+                                           'Master_TC_Time_Base,'
+                                           'Master_TC_Frame_Count,'
+                                           'Storage_Media_Serial_Number,'
+                                           'SMPTE_UMID,'
+                                           'Recorder_Type,'
+                                           'Image_Width,'
+                                           'Image_Height,'
+                                           'Active_Image_Width,'
+                                           'Active_Image_Height,'
+                                           'White_Balance,'
+                                           'Exposure_Index_ASA,'
+                                           'Target_Color_Space,'
+                                           'Lens_Model,'
+                                           'Lens_Serial_Number,'
+                                           'Lens_Distance_Unit,'
+                                           'Lens_Focus_Distance,'
+                                           'Lens_Focal_Length,'
+                                           'Lens_Iris,'
+                                           'S3D_Eye_Info,'
+                                           'RAWTYPE'
+                                           ') VALUES ('
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                                           '?, ?, ?, ?, ?, ?, ?, ?)',
+                                           (r3dlist[index/2 - 1], item[27], os.path.basename(r3dlist[index/2 - 1])[:15],
+                                            item[77], item[79], item[84], item[83],
+                                            item[81], item[86], '--', item[82], item[85],
+                                            os.path.basename(r3dlist[index/2 - 1]),
+                                            item[10], item[11], item[11], item[7], item[56], item[8], item[8],
+                                            item[14],
+                                            item[21], item[53] + ' ms', item[55], 'N/A', item[25], item[24], item[25],
+                                            123456,
+                                            item[13], '--', item[118], item[22],
+                                            item[23],
+                                            item[22], item[23],
+                                            item[33],
+                                            item[35], item[31], item[71], '--', 'mm', item[69], item[68], item[67],
+                                            item[117],
+                                            'r3d'))
+
+                    cursor.close()
+                    connect.commit()
+
+        except:
+            print('something error in sqliter3d method')
+        finally:
+            connect.close()
 
 
     def _initsqlitedb(self, dbpath):
@@ -87,7 +327,7 @@ class generaterawdb(object):
                 'CIRCLE_TAKE TEXT,'
                 'PRODUCTION_COMPANY TEXT,'
                 'LOCATION TEXT,'
-                'OPERATIR TEXT,'
+                'OPERATOR TEXT,'
                 'User_Info_1 TEXT,'
                 'User_Info_2 TEXT,'
                 'Camera_Clip_Name TEXT,'
@@ -185,7 +425,8 @@ class generaterawdb(object):
                 'Camera_Roll REAL,'
                 'Master_Slave_Setup_Info TEXT,'
                 'S3D_Eye_Info TEXT,'
-                'Sound_Roll TEXT'
+                'Sound_Roll TEXT,'
+                'RAWTYPE TEXT'
                 ');')
             cursor.close()
             connect.commit()
@@ -215,8 +456,9 @@ class generaterawdb(object):
 
         dbpath = os.path.join(os.path.expanduser('~'), 'Desktop', os.path.basename(self._scanpath) + '.db')
         self._initsqlitedb(dbpath)
-        self._symbolari(arilist)
         self._sqliteari(arilist, dbpath)
+
+        self._sqliter3d(r3dlist, dbpath)
 
         endtime = datetime.datetime.now()
         print(endtime - starttime)
