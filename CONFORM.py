5557050c9ce95bb7a3982bc17d906109ab35046a
gxagxagxa <gxagxagxa@me.com>
2015-06-04 22:53:32 +0800

    implement conform method, add save copylist method

--- CONFORM.py
+++ CONFORM.py
@@ -6,6 +6,8 @@ __author__ = 'andyguo'
 
 import sqlite3
 import EDL
+import os
+
 
 class CONFORM(object):
 
@@ -24,21 +26,46 @@ class CONFORM(object):
             connect = sqlite3.connect(db)
             cursor = connect.cursor()
 
-            for clip in EDLobject._edlclip:
-                cmd = 'SELECT * FROM RAWMETADATA ' \
-                      'WHERE ' \
-                      '(MASTER_TC >= \"%s\" AND END_TC <= \"%s\" AND ' \
-                      '(RAWTYPE = \"ari\" OR RAWTYPE = \"exr\" OR RAWTYPE = \"dng\" OR RAWTYPE = \"dpx\")) OR ' \
-                      '(MASTER_TC <= \"%s\" AND END_TC >= \"%s\" AND ' \
-                      '(RAWTYPE = \"mov\" OR RAWTYPE = \"mp4\" OR RAWTYPE = \"cine\" OR RAWTYPE = \"r3d\" OR RAWTYPE = \"mxf\"))' % \
-                      (clip['sourcein'], clip['sourceout'], clip['sourcein'], clip['sourceout'])
-                print(cmd)
+            try:
+                for clip in EDLobject._edlclip:
+                    cmd = 'SELECT * FROM RAWMETADATA ' \
+                          'WHERE ' \
+                          '(MASTER_TC >= \"%s\" AND END_TC <= \"%s\" AND ' \
+                          '(RAWTYPE = \"ari\" OR RAWTYPE = \"exr\" OR RAWTYPE = \"dng\" OR RAWTYPE = \"dpx\")) OR ' \
+                          '(MASTER_TC <= \"%s\" AND END_TC >= \"%s\" AND ' \
+                          '(RAWTYPE = \"mov\" OR RAWTYPE = \"mp4\" OR RAWTYPE = \"cine\" OR RAWTYPE = \"r3d\" OR RAWTYPE = \"mxf\"))' % \
+                          (clip['sourcein'], clip['sourceout'], clip['sourcein'], clip['sourceout'])
+                    if matchreel:
+                        if EDLusingreel == 'reel':
+                            cmd += ' AND (REEL LIKE "%s")' % (clip['reel'])
+                        if EDLusingreel == 'clipanme':
+                            cmd += ' AND (REEL LIKE "%s")' % (clip['clipname'])
+                        if EDLusingreel == 'longreel':
+                            cmd += ' AND (REEL LIKE "%s")' % (clip['longreel'])
 
-                cursor.execute(cmd)
-                matchedframes = cursor.fetchall()
-                print(matchedframes)
+                    # print(cmd)
+
+                    cursor.execute(cmd)
+                    matchedframes = cursor.fetchall()
+                    # print(matchedframes)
+                    clip['matched'] = matchedframes
 
 
+            except:
+                print('something error in conform')
+            finally:
+                connect.close()
+
+
+
+    def savecopylist(self, EDLobject, savepath):
+        with open(savepath, 'w') as f:
+            for index, clip in enumerate(EDLobject._edlclip):
+
+                if len(clip['matched']) > 0:
+                    linestring = clip['matched'][0][0]
+                    print(linestring)
+                    f.write(linestring+os.linesep)
 
 
 
@@ -54,4 +81,6 @@ if __name__ == '__main__':
     edlobject.readedl('/Users/andyguo/Desktop/Sequence 1.edl')
 
     conformobject = CONFORM()
-    conformobject.conform(edlobject, ['/Users/andyguo/Desktop/FOOTAGE.db', ])
+    conformobject.conform(edlobject, ['/Users/andyguo/Desktop/FOOTAGE.db', ], EDLusingreel='longreel')
+    # print(edlobject._edlclip)
+    conformobject.savecopylist(edlobject, '/Users/andyguo/Desktop/save.txt')
